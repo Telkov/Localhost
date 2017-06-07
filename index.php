fbd79f9ce38cefd271e5469a7bf990941031fdf4
Telkov <sanya_fl@ukr.net>
2017-06-07 10:53:35 +0300

    toggle added

--- index.php
+++ index.php
@@ -15,16 +15,6 @@ require_once "controllers/UserController.php";
 //require_once "views/layout.php";
 require_once "controllers/SavecommentController.php";
 ?>
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -33,6 +23,7 @@ require_once "controllers/SavecommentController.php";
 
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
+	<script type="text/javascript" src="js/jquery-1.10.2.js"></script>
 
 
 </head>
@@ -54,30 +45,37 @@ require_once "controllers/SavecommentController.php";
         <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
         </div>
     </div>
-
-    <div class="container-fluid">
-        <ul id="commentRoot">
-            <?php echo $comments;  ?>
-            <li id="newComment">
-                <div class="commentContent">
-                    <div id="cancelComment">X</div>
-                    <h6> <?php echo $_SESSION['user']['name'] ?> </h6>
-                    <div class="comment">
-                        Комментарий:
-                        <textarea name="newCommentText" maxlength="60"></textarea>
-                    </div>
-                    <button>Отправить</button>
-                    <!--                    <img class="loader" src="loader.gif">-->
-                </div>
-            </li>
-        </ul>
-    </div>
+    	
+    	<button class="btn btn-primary" id="show_hide">Show/Hide comments</button>
+	    <div class="container-fluid" id="main">
+	        <ul id="commentRoot">
+	            <?php echo $comments;  ?>
+	            <li id="newComment">
+	                <div class="commentContent">
+	                    <div id="cancelComment">X</div>
+	                    <h6> <?php echo $_SESSION['user']['name'] ?> </h6>
+	                    <div class="comment">
+	                        Комментарий:
+	                        <textarea name="newCommentText" maxlength="60"></textarea>
+	                    </div>
+	                    <button>Отправить</button>
+	                    <!--                    <img class="loader" src="loader.gif">-->
+	                </div>
+	            </li>
+	        </ul>
+	    </div>
+	    <script>
+			$( "#show_hide" ).click(function() {
+			  $( "#main" ).toggle( "slow" );
+			});
+		</script>
     <!--    <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>-->
 <!--    <script type="text/javascript" src="js/bootstrap.min.js"></script>-->
 <!--    <script type="text/javascript" src="js/npm.js"></script>-->
+	
     <script type="text/javascript" src="js/jquery-1.4.4.min.js"></script>
     <script type="text/javascript" src="js/comment.js"></script>
-
+	
 </body>
 </html>
 
